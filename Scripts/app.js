6e9eec4df6309ec9ed6928b3676ec433635fae6e
Brejanth Rajendran <Brejanth.rajendran@dcmail.ca>
2020-01-23 16:07:51 -0500

    Commit 3, Fixed 2(a) , and finished 2(b)

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,14 +1,31 @@
 "use strict";
 (function (){
-    
+    //2(a):
     //creates a variable for the products link
-    let LinkName = document.getElementsByTagName("a")[2];
+    let LinkName = document.getElementsByTagName("li")[1];
     // changes the text content from whatever it was to Projects
-    LinkName.textContent= "Projects" ;
+    LinkName.innerHTML ='<li id="products"class="nav-item"><a class="nav-link" href="#"><i class="fas fa-list"></i> Projects</a></li>' ;
    
  
-    
-    console.log(LinkName);
+    //Check what was changed
+    //console.log(LinkName);
+    //2(b):
+    // creates a varible that contains the Contact us link and its html
+    let Contactlink = document.getElementsByTagName("li")[5];
+
+    // use console to find out what li[5] is
+    //console.log(Contactlink);
+
+    //Clones the contactLink var to a new link
+    let NewLink = Contactlink.cloneNode(true);
+    //Changes the contact link from contact us to human Resources
+    NewLink.innerHTML = '<li  class="nav-item"><a class="nav-link" href="#"><i class="fas fa-user-circle"></i> Human Resources</a></li>';
    
+    //check for changes
+    //console.log(NewLink);
+   
+    //Insert NewLink before contact us link 
+    Contactlink.parentNode.insertBefore(NewLink,Contactlink);
+    
 
 })();    
