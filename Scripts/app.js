68a2dc70cd2f2de35dc37862f823c23f3891536e
Brejanth Rajendran <Brejanth.rajendran@dcmail.ca>
2020-01-22 19:01:35 -0500

    Commit 3, 2a

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+"use strict";
+(function (){
+    
+    //creates a variable for the products link
+    let LinkName = document.getElementsByTagName("a")[2];
+    // changes the text content from whatever it was to Projects
+    LinkName.textContent= "Projects" ;
+   
+ 
+    
+    console.log(LinkName);
+   
+
+})();    
